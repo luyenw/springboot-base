acc761413e609c28b9812c10a10396186f71ecb0
vinh412 <vinhduong412@gmail.com>
2024-11-05 17:09:06 +0700

    init folder uploads before run

--- src/main/java/com/canifa/stylenest/StylenestApplication.java
+++ src/main/java/com/canifa/stylenest/StylenestApplication.java
@@ -1,13 +1,23 @@
 package com.canifa.stylenest;
 
+import com.canifa.stylenest.service.StorageService;
+import jakarta.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StylenestApplication {
+public class StylenestApplication implements CommandLineRunner {
+
+	@Resource
+	StorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StylenestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		storageService.init();
+	}
 }
